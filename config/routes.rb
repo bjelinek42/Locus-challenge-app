f54b8376f9e07b7de75b422a02936e5e757213a3
ben <bjelinek42@gmail.com>
2022-06-28 16:32:03 -0600

    add route comment

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :tasks, except: :show
   end
 
+  #I assume I could also remove the "except: :show" part as well as what I put below
+
   get "/task_lists/:id" => "task_lists#show"
   patch "/task_lists/:id" => "task_lists#update"
 end
